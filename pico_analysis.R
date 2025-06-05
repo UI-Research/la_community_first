2f0344d013e7678078bed7096b4621ebb089070e
gsamuelsurban <gsamuels@urban.org>
2025-06-05 16:15:48 -0400

    adding own box

--- pico_analysis.R
+++ pico_analysis.R
@@ -1,5 +1,7 @@
 ### Load the script for pico data ###
 source("C:/Users/tmaginn/Documents/GitHub/la_community_first/data_pull.R")
+source("C:/Users/gsamuels/Documents/GitHub/la_community_first/data_pull.R")
+
 
 
 # Set working directory and file paths
@@ -13,6 +15,8 @@ pico_master_data <- clean_and_prepare_data(file_path)
 ### same thing as above but now loading in script for LA city data ###
 
 source("C:/Users/tmaginn/Documents/GitHub/la_community_first/la_city_data_pull.R")
+source("C:/Users/gsamuels/Documents/GitHub/la_community_first/la_city_data_pull.R")
+
 
 username <- getwd() %>% str_split("\\/") %>% unlist() %>% .[3]
 file_path <- file.path("C:", "Users", username, "Box", "LA Transit project/Social Climate Analysis")
